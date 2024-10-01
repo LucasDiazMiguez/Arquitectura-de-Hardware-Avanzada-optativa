01187663ece0f2d042d19c69eed02c4ec10c0982
LucasDiazMiguez <ldiazmiguez@emtech.com.ar>
2024-10-01 00:27:37 -0300

    Running instruction list in RISC V PIPELINE

--- Pipelined_w_Hazard/riscv_base_instruction_list_running.sv
+++ Pipelined_w_Hazard/riscv_base_instruction_list_running.sv
@@ -1,24 +1,44 @@
+// * ADDI
+// * ANDI
+// * ORI
+// * OR
+// * AND
+// * SW
+// * LW
+// * ADD
+// * SUB
+//? SLT
+//? BEQ
+
 //*Suman 2 del reg 1 al 12
 
-00200093//ADDI x1, x0, 1
-00200113//ADDI x2, x0, 2
-00200193//ADDI x3, x0, 3
-00200213//ADDI x4, x0, 4
-00200313//ADDI x5, x0, 5
-00200393//ADDI x6, x0, 6
-00200413//ADDI x7, x0, 7
-00200493//ADDI x8, x0, 8
-00200513//ADDI x9, x0, 9
-00200593//ADDI x10, x0, 10
-00200613//ADDI x11, x0, 11
+00200093 //ADDI x1, x0, 2
+00200113 //ADDI x2, x0, 2
+00200193  //ADDI x3, x0, 2 
+00200213  //ADDI x4, x0, 2 
+00200293  //ADDI x5, x0, 2
+00200313  //ADDI x6, x0, 2
+00200393  //ADDI x7, x0, 2
+00200413  //ADDI x8, x0, 2
+00200493  //ADDI x9, x0, 2
+00200513 //ADDI x10, x0, 2
+00200593 //ADDI x11, x0, 2
+//*Suman 3....D del reg 1 al 12
 
+00200093//ADDI x1, x0, 2
+00300093//ADDI x2, x0, 3
+00400113//ADDI x3, x0, 4
+00500193//ADDI x4, x0, 5
+00600213//ADDI x5, x0, 6
+00700293//ADDI x6, x0, 7
+00800313//ADDI x7, x0, 8
+00900393//ADDI x8, x0, 9
+00A00413//ADDI x9, x0, A
+00B00493//ADDI x10, x0, B
+00C00513//ADDI x11, x0, C
+00D00593//ADDI x12, x0, D
 ///////////111///////
-
-01400093 //* addi x10, x1, 20 
-
-
-/////////////////
-
+//*working
 00000093 //ADDI x1, x0, 0
 00000113 //ADDI x2, x0, 0
 00000193 //ADDI x3, x0, 0
@@ -62,7 +82,7 @@
 00810893   // ori x17, x8, 8
 00910913   // ori x18, x9, 9
 00A10993   // ori x19, x10, 10
-//*
+//* OR GOOOD
 0023E2B3  // or x7,  x7, x2    # x7 = (x7 OR x2)
 0023E3B3 // or x8,  x7, x2    # x8 = (x7 OR x2)
 0023E4B3 // or x9,  x7, x2    # x9 = (x7 OR x2)
@@ -75,6 +95,91 @@
 00326E33 // or x28, x4, x3    # x28 = (x4 OR x3)
 00326EB3 // or x29, x4, x3    # x29 = (x4 OR x3)
 00326F33 // or x30, x4, x3    # x30 = (x4 OR x3)
+//* store woords
+00202023 //SW x2,0 (x0)
+00202223 //SW x2,4 (x0)
+00202423 //SW x2,8 (x0)
+00202623 //SW x2,12 (x0)
+00202823 //SW x2,16 (x0)
+00202A23 //SW x2,20 (x0)
+00202C23 //SW x2,24 (x0)
+00202E23 //SW x2,28 (x0)
+02202023 //SW x2,32 (x0)
+02202223 //SW x2,36 (x0)
+02202423 //SW x2,40 (x0)
+02202623 //SW x2,44 (x0)
+02202823 //SW x2,48 (x0)
+02202A23 //SW x2,52 (x0)
+02202C23 //SW x2,56 (x0)
+02202E23 //SW x2,60 (x0)
+
+
+
+
+//**LW
+00002083  // guardo el valor ram(0) en el registro 1  del rf
+00402103  // guardo el valor ram(4) en el registro 2  del rf
+00802183  // guardo el valor ram(8) en el registro 3  del rf
+00C02203  // guardo el valor ram(12) en el registro 4  del rf
+01002283  // guardo el valor ram(16) en el registro 5  del rf
+01402303  // guardo el valor ram(20) en el registro 6  del rf
+01802383  // guardo el valor ram(24) en el registro 7  del rf
+01C02403  // guardo el valor ram(28) en el registro 8  del rf
+02002483  // guardo el valor ram(32) en el registro 9  del rf
+02402503  // guardo el valor ram(36) en el registro 10  del rf
+02802583  // guardo el valor ram(40) en el registro 11  del rf
+02C02603  // guardo el valor ram(44) en el registro 12 del rf
+
+
+
+//*** ADDD
+
+00900B33 // ADD x22, x0, x9	x22 = x0 + x9
+00908BB3 // ADD x23, x1, x9	x23 = x1 + x9
+00910C33 // ADD x24, x2, x9	x24 = x2 + x9
+00918CB3 // ADD x25, x3, x9	x25 = x3 + x9
+00920D33 // ADD x26, x4, x9	x26 = x4 + x9
+00928DB3 // ADD x27, x5, x9	x27 = x5 + x9
+00930E33 // ADD x28, x6, x9	x28 = x6 + x9
+00938EB3 // ADD x29, x7, x9	x29 = x7 + x9
+00940F33 // ADD x30, x8, x9	x30 = x8 + x9
+
+//*** SUBS
+40900B33 // SUBS x22, x0, x9	x22 = x0 - x9
+40908BB3 // SUBS x23, x1, x9	x23 = x1 - x9
+40910C33 // SUBS x24, x2, x9	x24 = x2 - x9
+40918CB3 // SUBS x25, x3, x9	x25 = x3 - x9
+40920D33 // SUBS x26, x4, x9	x26 = x4 - x9
+40928DB3 // SUBS x27, x5, x9	x27 = x5 - x9
+40930E33 // SUBS x28, x6, x9	x28 = x6 - x9
+40938EB3 // SUBS x29, x7, x9	x29 = x7 - x9
+40940F33 // SUBS x30, x8, x9	x30 = x8 - x9
+
+//****** SLT
+002B2633 //compara x2 con x22 y guardo en x12 SLT x12,x2,x22
+016126B3 //compara x22 con x2 y guardo en x13 
+003BA733 // compara x3 con x23 y guardo en x14 
+0171A7B3 // compara x23 con x3 y guardo en x15 
+004C2833 // compara x4 con x24 y guardo en x16 
+018228B3 // compara x24 con x4 y guardo en x17 
+
+
+//********beqn
+
+// ? WORKKKKKKKKKKK
+
+0000000-00010-10110-010-01100-0110011 // compara x2 con x22 y guardo en x12 SLT x12,x2,x22
+0000000-10110-00010-010-01101-0110011 // compara x
+
+0000000-00011-10111-010-01110-0110011 // compara x3 con x23 y guardo en x14 
+0000000-10111-00011-010-01111-0110011 // compara x23 con x3 y guardo en x15 
+00000000001110111010011100110011 // compara x3 con x23 y guardo en x14 
+00000001011100011010011110110011 // compara x23 con x3 y guardo en x15 
+
+0000000-00100-11000-010-10000-0110011 // compara x4 con x24 y guardo en x16 
+0000000-11000-00100-010-10001-0110011 // compara x24 con x4 y guardo en x17 
+00000000010011000010100000110011 // compara x4 con x24 y guardo en x16 
+00000001100000100010100010110011 // compara x24 con x4 y guardo en x17 
 
 
 
@@ -85,6 +190,38 @@
 
 
 
+0000000-00011-11000-010-01110-0110011 // compara
+0000000-11000-00011-010-01111-0110011 // compara x
+00000000001111000010011100110011 // compara
+00000001100000011010011110110011 // compara x
+
+0000000-00100-11010-010-10000-0110011 // compara x
+0000000-11010-00100-010-10001-0110011 // compara x
+00000000010011010010100000110011 // compara x
+00000001101000100010100010110011 // compara x
+
+0000000-00101-11001-010-10010-0110011 // compara x
+0000000-11001-00101-010-10011-0110011 // compara x
+00000000010111001010100100110011 // compara x
+00000001100100101010100110110011 // compara x
+
+0000000-00110-11011-010-10100-0110011 // compara x
+0000000-11011-00110-010-10101-0110011 // compara x
+00000000011011011010101000110011 // compara x
+00000001101100110010101010110011 // compara x
+
+
+
+
+010110
+010111
+011000
+011001
+011010
+011011
+011100
+011101
+011110
 
 
 
@@ -94,8 +231,70 @@
 
 
 
-
-
+000000 - 0
+000001 - 1
+000010 - 2
+000011 - 3
+000100 - 4
+000101 - 5
+000110 - 6
+000111 - 7
+001000 - 8
+001001 - 9
+001010 - 10
+001011 - 11
+001100 - 12
+001101 - 13
+001110 - 14
+001111 - 15
+010000 - 16
+010001 - 17
+010010 - 18
+010011 - 19
+010100 - 20
+010101 - 21
+010110 - 22
+010111 - 23
+011000 - 24
+011001 - 25
+011010 - 26
+011011 - 27
+011100 - 28
+011101 - 29
+011110 - 30
+011111 - 31
+100000 - 32
+100001 - 33
+100010 - 34
+100011 - 35
+100100 - 36
+100101 - 37
+100110 - 38
+100111 - 39
+101000 - 40
+101001 - 41
+101010 - 42
+101011 - 43
+101100 - 44
+101101 - 45
+101110 - 46
+101111 - 47
+110000 - 48
+110001 - 49
+110010 - 50
+110011 - 51
+110100 - 52
+110101 - 53
+110110 - 54
+110111 - 55
+111000 - 56
+111001 - 57
+111010 - 58
+111011 - 59
+111100 - 60
+111101 - 61
+111110 - 62
+111111 - 63
 //!---------------------------No andan por bug del rd = al inmediatio que no tiene sentido
 00100093 // addi x1, x0, 1
 00200113 // addi x2, x0, 2
