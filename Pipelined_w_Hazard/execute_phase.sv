bb9eccfd8afac8da874dc382daa92a9bf6040f69
LucasDiazMiguez <ldiazmiguez@emtech.com.ar>
2024-10-01 00:26:46 -0300

    expanded declarations in  execute_phase

--- Pipelined_w_Hazard/execute_phase.sv
+++ Pipelined_w_Hazard/execute_phase.sv
@@ -2,14 +2,20 @@ module execute_phase (
     input logic clk,
     input logic [31:0] RD1,
     input logic [31:0] RD2,
-    input logic [31:0] PCD,ImmExt_D,PC_Plus4D,
+    input logic [31:0] PCD,
+    input logic [31:0] ImmExt_D,
+    input logic [31:0] PC_Plus4D,
     input logic [4:0] RdD,
     input logic [4:0] Rs1D,
     input logic [4:0] Rs2D,
     output  logic [4:0] RdE,
     output  logic [4:0] RSD1_E,
     output  logic [4:0] RSD2_E,
-    output  logic [31:0] RD1E,RD2E,PCE,ImmExt_E,PC_Plus4E
+    output  logic [31:0] RD1E,
+    output  logic [31:0] RD2E,
+    output  logic [31:0] PCE,
+    output  logic [31:0] ImmExt_E,
+    output  logic [31:0] PC_Plus4E
 );
 always_ff @(posedge clk)
     begin
