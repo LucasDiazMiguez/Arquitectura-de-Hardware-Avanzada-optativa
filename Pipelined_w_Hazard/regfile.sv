66809ec734bfc7b7d32212e007b301e62db5af73
LucasDiazMiguez <ldiazmiguez@emtech.com.ar>
2024-10-01 00:27:17 -0300

    fix nenedge clock in RF

--- Pipelined_w_Hazard/regfile.sv
+++ Pipelined_w_Hazard/regfile.sv
@@ -11,7 +11,7 @@ module regfile (
   // read two ports combinationally (A1/RD1, A2/RD2)
   // write third port on rising edge of clock (A3/WD3/WE3)
   // register 0 hardwired to 0
-  always_ff @(posedge clk) if (we3) rf[a3] <= wd3;
+  always_ff @(negedge clk) if (we3) rf[a3] <= wd3;
   assign rd1 = (a1 != 0) ? rf[a1] : 0;
   assign rd2 = (a2 != 0) ? rf[a2] : 0;
 endmodule
